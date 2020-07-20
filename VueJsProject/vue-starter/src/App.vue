d2b4a9aa87691a911bfd77a88d6fbaaa378cd91e
= <54645976+yazeedalota@users.noreply.github.com>
2020-07-20 22:05:04 +0300

    First Block of Projects

---
+++ VueJsProject/vue-starter/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   <NavBar/>
+    <div class="container">
+      <router-view/>
+    </div>
+ </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+
+export default {
+  name: "app",
+
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ VueJsProject/vue-starter/src/PostService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev';
+
+export default class PostService {
+    getAllPosts(){
+        return axios.get('/posts');
+    }
+
+    getPost(number){
+        return axios.get(`/posts/${number}`);
+    }
+
+    writePost(post){
+        if(post.id){
+            return axios.put(`/post/${post.id}`, post);
+        } else return axios.post('/post', post);
+    }
+
+
+    deletePost(id){
+        return axios.delete(`/post/${id}`);
+    }
+} 
